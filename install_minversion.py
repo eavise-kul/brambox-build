2b09ba8c6ff83745574a5e3b3b285ac159d817f7
0phoff <0phoff@users.noreply.github.com>
2020-08-21 10:30:05 +0200

    Debug travis - 7

---
+++ install_minversion.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import os
+import argparse
+import requests
+from fnmatch import fnmatch
+from distutils.version import LooseVersion
+
+def get_version(package, minversion=None):
+    pypi_data = requests.get(f'https://pypi.org/pypi/{package}/json').json()
+
+    # Get rid of pre-releases and versions smaller than minversion
+    versions = []
+    for version in pypi_data['releases']:
+        try:
+            [int(x) for x in version.split('.')]
+        except ValueError:
+            continue
+        else:
+            if minversion is None or LooseVersion(version) >= minversion:
+                versions.append(version)
+
+    # Find correct version
+
+
+if __name__ == '__main__':
+    #parser = argparse.ArgumentParser('Install correct minimal version of a dependency, according to platform and python')
+    #parser.add_argument('--arch', required=True, choices=['mac', 'linux32', 'linux64', 'armv8', 'windows32', 'windows64'])
+    #parser.add_argument('package', nargs='+', metavar="packagename[=minversion]")
+    #args = parser.parse_args()
+
+    print('ENV\n===')
+    print((os.environ.keys()).join('\n'))
