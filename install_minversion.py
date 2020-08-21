76c51f92db247cc8e1835819c5a1c57fd8cc321a
0phoff <0phoff@users.noreply.github.com>
2020-08-21 10:53:01 +0200

    Debug travis - 14

--- install_minversion.py
+++ install_minversion.py
@@ -2,7 +2,7 @@
 import os
 import argparse
 import requests
-from fnmatch import fnmatch
+from wheel.pep425tags import get_abi_tag
 from distutils.version import LooseVersion
 
 def get_version(package, minversion=None):
@@ -24,10 +24,13 @@ def get_version(package, minversion=None):
 
 if __name__ == '__main__':
     #parser = argparse.ArgumentParser('Install correct minimal version of a dependency, according to platform and python')
-    #parser.add_argument('--arch', required=True, choices=['mac', 'linux32', 'linux64', 'armv8', 'windows32', 'windows64'])
     #parser.add_argument('package', nargs='+', metavar="packagename[=minversion]")
     #args = parser.parse_args()
 
-    print('ENV\n===')
-    print('\n'.join([f'{k}={v}' for k,v in os.environ.items()]))
+    abi = get_abi_tag()
+    platform = os.environ['AUDITWHEEL_PLAT']
+
+    print('===')
+    print(abi)
+    print(platform)
     print('===')
