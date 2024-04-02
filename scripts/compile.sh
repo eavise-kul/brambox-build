94c7cfdbc1f9f87b1e75a03215b8880ddabbe5b3
0phoff <0phoff@users.noreply.github.com>
2024-04-02 16:39:03 +0200

    Force the usage of cython3

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -7,19 +7,19 @@ set -x
 # Install correct dependencies depending on version
 version=$(python --version | cut -d' ' -f2)
 if [[ $version == 3.6* ]]; then
-    python -m pip install numpy==1.13.3 cython==0.29.16
+    python -m pip install numpy==1.13.3 cython>=3.0
 elif [[ $version == 3.7* ]]; then
-    python -m pip install numpy==1.14.6 cython==0.29.16
+    python -m pip install numpy==1.14.6 cython>=3.0
 elif [[ $version == 3.8* ]]; then
-    python -m pip install numpy==1.17.5 cython==0.29.16
+    python -m pip install numpy==1.17.5 cython>=3.0
 elif [[ $version == 3.9* ]]; then
-    python -m pip install numpy==1.19.3 cython==0.29.21
+    python -m pip install numpy==1.19.3 cython>=3.0
 elif [[ $version == 3.10* ]]; then
-    python -m pip install numpy==1.21.6 cython==0.29.25
+    python -m pip install numpy==1.21.6 cython>=3.0
 elif [[ $version == 3.11* ]]; then
-    python -m pip install numpy==1.23.2 cython==0.29.30
+    python -m pip install numpy==1.23.2 cython>=3.0
 elif [[ $version == 3.12* ]]; then
-    python -m pip install numpy==1.26.2 cython==0.29.37
+    python -m pip install numpy==1.26.2 cython>=3.0
 else
     echo "Unsupported python version: $version"
     exit 1
