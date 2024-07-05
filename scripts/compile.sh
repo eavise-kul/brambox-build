b289b5b575f022ad146e955f0ce4a1f272a97856
too7qfn <tanguy.ophoff@colruytgroup.com>
2024-07-05 15:40:26 +0200

    Install tomli if python < 3.11

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -8,15 +8,15 @@ set -x
 pip cache purge
 version=$(python --version | cut -d' ' -f2)
 if [[ $version == 3.6* ]]; then
-    python -m pip install "numpy==1.13.3" "cython~=3.0.0"
+    python -m pip install "numpy==1.13.3" "cython~=3.0.0" tomli
 elif [[ $version == 3.7* ]]; then
-    python -m pip install "numpy==1.14.6" "cython~=3.0.0"
+    python -m pip install "numpy==1.14.6" "cython~=3.0.0" tomli
 elif [[ $version == 3.8* ]]; then
-    python -m pip install "numpy==1.17.5" "cython~=3.0.0"
+    python -m pip install "numpy==1.17.5" "cython~=3.0.0" tomli
 elif [[ $version == 3.9* ]]; then
-    python -m pip install "numpy==1.19.3" "cython~=3.0.0"
+    python -m pip install "numpy==1.19.3" "cython~=3.0.0" tomli
 elif [[ $version == 3.10* ]]; then
-    python -m pip install "numpy==1.21.6" "cython~=3.0.0"
+    python -m pip install "numpy==1.21.6" "cython~=3.0.0" tomli
 elif [[ $version == 3.11* ]]; then
     python -m pip install "numpy==1.23.2" "cython~=3.0.0"
 elif [[ $version == 3.12* ]]; then
@@ -30,7 +30,6 @@ fi
 cd "$1"
 
 # Clean project
-python setup.py clean --all
 find brambox -type f -name '*.c' -exec rm {} \;
 find brambox -type f -name '*.cpp' -exec rm {} \;
 find brambox -type f -name '*.so' -exec rm {} \;
